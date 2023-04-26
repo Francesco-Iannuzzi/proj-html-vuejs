0f576fad5fca809ec0ddc58f7c8b97045fce51c4
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-26 13:25:20 +0200

    first step header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -13,7 +13,50 @@ export default {
 
 <template>
     <header>
+        <div class="container">
+            <nav class="navbar navbar-expand">
+                <div class="container">
+                    <div class="logo">
+                        <a class="navbar-brand d-flex" href="#">
+                            <img src=".././assets/img/cropped-cropped-icon-270x270.png" alt="Phlox_Corporate_logo"
+                                width="40" height="40">
+                            <div class="ms-2">
+                                <h4>Phlox</h4>
+                                <h4>Corporate</h4>
+                            </div>
+                        </a>
+                    </div>
+                    <!-- /logo -->
 
+                    <div class="navbar" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Portfolio</a>
+                            </li>
+                        </ul>
+                        <!-- /menu sections -->
+                        <button class="btn border-0" type="submit">Sign In</button>
+                        <!-- /sign In button -->
+                    </div>
+                    <!-- /navbar -->
+                </div>
+            </nav>
+        </div>
     </header>
     <!-- /header -->
 </template>
