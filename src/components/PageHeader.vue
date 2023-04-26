463977c1dcc5f08a8e275223ffdfcf44f1351c2e
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-26 15:18:17 +0200

    add jumbo header + first step section our service

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -22,7 +22,7 @@ export default {
                 <div class="container">
                     <LogoElement></LogoElement>
                     <!-- /logo -->
-                    <div class="navbar" id="navbarSupportedContent">
+                    <div class="menu_nav navbar" id="navbarSupportedContent">
                         <ul class="navbar-nav me-4 mb-2 mb-lg-0">
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Home</a>
@@ -44,12 +44,45 @@ export default {
                             </li>
                         </ul>
                         <!-- /menu sections -->
-                        <button class="btn border-0 rounded-5 px-4">Sign In</button>
+                        <button class="btn_primary btn border-0 rounded-5 px-4">Sign In</button>
                         <!-- /sign In button -->
                     </div>
-                    <!-- /navbar -->
+                    <!-- /navbar menu -->
                 </div>
             </nav>
+            <!-- /navbar -->
+            <section class="jumbo">
+                <div class="row pb-5">
+                    <div class="jumbo_left col-6">
+                        <div class="pb-4">
+                            <h1>
+                                Business Planning
+                            </h1>
+                            <h1 class="fw-light">
+                                Digital Technology
+                            </h1>
+                            <h1>
+                                Modern Solution!
+                            </h1>
+                        </div>
+                        <p>
+                            When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper
+                            surface
+                        </p>
+                        <div class="pt-4">
+                            <button class="btn_secondary btn border-0 rounded-5 px-4 me-4">Try It Now</button>
+                            <button class="btn_primary btn border-0 rounded-5 px-4">Explore More</button>
+                        </div>
+                    </div>
+                    <!-- /jumbo_left -->
+
+                    <div class="jumbo_right col-6 py-5">
+                        <img class="img-fluid" src="../assets/img/header.png" alt="">
+                    </div>
+                    <!-- /jumbo_right -->
+                </div>
+            </section>
+            <!-- /jumbo -->
         </div>
     </header>
     <!-- /header -->
--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -13,7 +13,39 @@ export default {
 
 <template>
     <main>
-
+        <section class="our_service py-5">
+            <div class="container">
+                <div class="our_service_title text-center">
+                    <h5>
+                        Our Services
+                    </h5>
+                    <div class="d-flex justify-content-center">
+                        <h2>
+                            <strong>What</strong> We Do
+                        </h2>
+                    </div>
+                    <p>
+                        When, while the lovely valley teems with vapour around meand the
+                    </p>
+                </div>
+                <!-- /our_service_title -->
+                <div class="our_service_item row">
+                    <div class="col">
+                        <div class="card text-center border-0">
+                            <img class="img-fluid" src="../assets/img/Group-247.png" alt="">
+                            <h3 class="my-4">Data Analysis</h3>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                    <div class="col"></div>
+                </div>
+            </div>
+        </section>
     </main>
     <!-- /main -->
 </template>
