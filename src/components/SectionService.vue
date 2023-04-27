d304179011f250c534ff45a1ac8cd464001bde11
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-27 14:47:18 +0200

    add effect hover to SectionService

--- src/components/SectionService.vue
+++ src/components/SectionService.vue
@@ -29,54 +29,78 @@ export default {
 
             <div class="our_services_items row py-5">
                 <div class="col">
-                    <div class="card text-center border-0">
-                        <div class="container">
-                            <img class="img-fluid" src="../assets/img/Group-247.png" alt="data_analysis">
+                    <div class="item_card card text-center border-0">
+                        <div class="item_card_img container">
+                            <div class="card py-2 border-0 rounded-4">
+                                <img width="120" src="../assets/img/Group-247.png" alt="data_analysis">
+                            </div>
                         </div>
-                        <h3 class="my-4">Data Analysis</h3>
-                        <p>
-                            When, while the lovely valley teems with vapour around meand the meridian sun strikes the
-                            upper
-                        </p>
+                        <!-- /item_card_img -->
+                        <div class="item_card_text card border-0 rounded-4 pt-3">
+                            <h3 class="my-4">Data Analysis</h3>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper
+                            </p>
+                        </div>
+                        <!-- /item_card_text -->
                     </div>
                 </div>
                 <!-- /col -->
                 <div class="col">
-                    <div class="card text-center border-0">
-                        <div class="container">
-                            <img class="img-fluid" src="../assets/img/Group-567.png" alt="data_analysis">
+                    <div class="item_card card text-center border-0">
+                        <div class="item_card_img container">
+                            <div class="card py-2 border-0 rounded-4">
+                                <img width="120" src="../assets/img/Group-567.png" alt="data_analysis">
+                            </div>
                         </div>
-                        <h3 class="my-4">Data Analysis</h3>
-                        <p>
-                            When, while the lovely valley teems with vapour around meand the meridian sun strikes the
-                            upper
-                        </p>
+                        <!-- /item_card_img -->
+                        <div class="item_card_text card border-0 rounded-4 pt-3">
+                            <h3 class="my-4">SEO Optimization</h3>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper
+                            </p>
+                        </div>
+                        <!-- /item_card_text -->
                     </div>
                 </div>
                 <!-- /col -->
                 <div class="col">
-                    <div class="card text-center border-0">
-                        <div class="container">
-                            <img class="img-fluid" src="../assets/img/Group-538.png" alt="data_analysis">
+                    <div class="item_card card text-center border-0">
+                        <div class="item_card_img container">
+                            <div class="card py-2 border-0 rounded-4">
+                                <img width="120" src="../assets/img/Group-538.png" alt="data_analysis">
+                            </div>
                         </div>
-                        <h3 class="my-4">Data Analysis</h3>
-                        <p>
-                            When, while the lovely valley teems with vapour around meand the meridian sun strikes the
-                            upper
-                        </p>
+                        <!-- /item_card_img -->
+                        <div class="item_card_text card border-0 rounded-4 pt-3">
+                            <h3 class="my-4">Security Data</h3>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper
+                            </p>
+                        </div>
+                        <!-- /item_card_text -->
                     </div>
                 </div>
                 <!-- /col -->
                 <div class="col">
-                    <div class="card text-center border-0">
-                        <div class="container">
-                            <img class="img-fluid" src="../assets/img/Group-566.png" alt="data_analysis">
+                    <div class="item_card card text-center border-0">
+                        <div class="item_card_img container">
+                            <div class="card py-2 border-0 rounded-4">
+                                <img width="120" src="../assets/img/Group-566.png" alt="data_analysis">
+                            </div>
                         </div>
-                        <h3 class="my-4">Data Analysis</h3>
-                        <p>
-                            When, while the lovely valley teems with vapour around meand the meridian sun strikes the
-                            upper
-                        </p>
+                        <!-- /item_card_img -->
+                        <div class="item_card_text card border-0 rounded-4 pt-3">
+                            <h3 class="my-4">Branding Strategy</h3>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper
+                            </p>
+                        </div>
+                        <!-- /item_card_text -->
                     </div>
                 </div>
                 <!-- /col -->
