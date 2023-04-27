7274459f3feb470884562c04202066039da621db
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-27 09:58:07 +0200

    add first step testimonal section

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -35,6 +35,91 @@ export default {
         <!-- /price_list -->
         <SectionBlog></SectionBlog>
         <!-- /blog -->
+        <section class="testimonial py-5">
+            <div class="container">
+                <div class="testimonial_title text-center">
+                    <h5>
+                        Testimonials
+                    </h5>
+                    <div class="d-flex justify-content-center">
+                        <h2>
+                            <strong>What</strong> People Say
+                        </h2>
+                    </div>
+                    <p>
+                        When, while the lovely valley teems with vapour around meand the meridian sun strikes the upper
+                        surface.
+                    </p>
+                </div>
+                <!-- /testimonial_title -->
+                <div class="testimonial_staff row">
+                    <div class="col">
+                        <div class="card border-0 text-center">
+                            <div class="container">
+                                <img class="img-fluid w-25" src="../assets/img/szabo-viktor-1266895-unsplash-1024x1024.png"
+                                    alt="">
+                            </div>
+                            <span class="star py-5"> &starf; &starf; &starf; &star; &star; </span>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper impenetrable foliage of my trees, and but thousand.
+                            </p>
+                            <h5 class="pt-3">
+                                <strong>Michkel Anegl</strong>
+                            </h5>
+                            <span>Marketing Management</span>
+                            <div class="cit_bg">
+                                <h1>"</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /col -->
+                    <div class="col">
+                        <div class="card border-0 text-center">
+                            <div class="container">
+                                <img class="img-fluid w-25"
+                                    src="../assets/img/philipe-cavalcante-539505-unsplash-1024x1024.png" alt="">
+                            </div>
+                            <span class="star py-5"> &starf; &starf; &starf; &star; &star; </span>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper impenetrable foliage of my trees, and but thousand.
+                            </p>
+                            <h5 class="pt-3">
+                                <strong>Michkel Anegl</strong>
+                            </h5>
+                            <span>Marketing Management</span>
+                            <div class="cit_bg">
+                                <h1>"</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /col -->
+                    <div class="col">
+                        <div class="card border-0 text-center">
+                            <div class="container">
+                                <img class="img-fluid w-25" src="../assets/img/fabio-spinelli-695744-unsplash-1024x1024.png"
+                                    alt="">
+                            </div>
+                            <span class="star py-5"> &starf; &starf; &starf; &star; &star; </span>
+                            <p>
+                                When, while the lovely valley teems with vapour around meand the meridian sun strikes the
+                                upper impenetrable foliage of my trees, and but thousand.
+                            </p>
+                            <h5 class="pt-3">
+                                <strong>Michkel Anegl</strong>
+                            </h5>
+                            <span>Marketing Management</span>
+                            <div class="cit_bg">
+                                <h1>"</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /col -->
+                </div>
+            </div>
+        </section>
+        <!-- /testimonial -->
 
     </main>
     <!-- /main -->
