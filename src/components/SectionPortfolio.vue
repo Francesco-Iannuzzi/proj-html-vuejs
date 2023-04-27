108f86d75aa9b1b1192d9a82aaada4b02769aba7
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-04-27 19:38:46 +0200

    first step section portfolio carousel(static)

--- src/components/SectionPortfolio.vue
+++ src/components/SectionPortfolio.vue
@@ -33,9 +33,51 @@ export default {
         </div>
         <!-- /portfolio_top -->
 
-        <div class="portfolio_bottom">
-            <div class="carousel d-flex">
-                <!-- to do a carousel -->
+        <div class="portfolio_bottom container-fluid">
+            <div class="carousel row">
+                <div class="col">
+                    <div class="carousel_img card border-0 rounded-5">
+                        <img class="img-fluid" src="../assets/img/84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg"
+                            alt="">
+                    </div>
+                    <div class="carousel_text card border-0 flex-row justify-content-between">
+                        <h5 class="m-0">Basket of flower on table</h5>
+                        <span>Branding Strategy</span>
+                    </div>
+                </div>
+                <!-- /col -->
+                <div class="col">
+                    <div class="carousel_img card border-0 rounded-5">
+                        <img class="img-fluid" src="../assets/img/DRY-1-790x576.jpg" alt="">
+                    </div>
+                    <div class="carousel_text card border-0 flex-row justify-content-between">
+                        <h5 class="m-0">Basket of flower on table</h5>
+                        <span>Branding Strategy</span>
+                    </div>
+                </div>
+                <!-- /col -->
+                <div class="col">
+                    <div class="carousel_img card border-0 rounded-5">
+                        <img class="img-fluid" src="../assets/img/8wa60okr-1-790x576.jpg" alt="">
+                    </div>
+                    <div class="carousel_text card border-0 flex-row justify-content-between">
+                        <h5 class="m-0">Basket of flower on table</h5>
+                        <span>Branding Strategy</span>
+                    </div>
+                </div>
+                <!-- /col -->
+                <div class="col">
+                    <div class="carousel_img card border-0 rounded-5">
+                        <img class="img-fluid" src="../assets/img/a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg"
+                            alt="">
+                    </div>
+                    <div class="carousel_text card border-0 flex-row justify-content-between">
+                        <h5 class="m-0">Basket of flower on table</h5>
+                        <span>Branding Strategy</span>
+                    </div>
+                </div>
+                <!-- /col -->
+
             </div>
         </div>
         <!-- /portfolio_bottom -->
